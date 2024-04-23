5432b475ea70b8a794c64f238ffff6ad1fbcb790
Vijay Patel <spiritedtechie@gmail.com>
2024-04-23 09:50:33 +0100

    Adds cleaner for loop / iterator

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,8 +14,7 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            Item item = items[i];
+        for (Item item : this.items) {
 
             if (item.name.equals(SULFURAS)) {
                 continue;
